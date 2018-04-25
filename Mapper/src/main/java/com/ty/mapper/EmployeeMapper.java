264f5400b0043d03cecf560282a081d8413225b1
ty <465141730@qq.com>
2018-04-25 11:33:39 +0800

    Basic

---
+++ Mapper/src/main/java/com/ty/mapper/EmployeeMapper.java
@@ -0,0 +1,17 @@
+package com.ty.mapper;
+
+import com.ty.entities.Employee;
+
+import tk.mybatis.mapper.common.Mapper;
+/**
+ * 
+* @Description: 具体操作数据库的mapper接口
+*
+* @ClassName: EmployeeMapper.java
+* @version: v1.0.0
+* @author: 
+* @date: 2018年4月24日 下午2:18:44
+ */
+public interface EmployeeMapper extends Mapper<Employee>{
+	
+}
